30ac7d2192e07010cc2d84c44f5304ac4842496f
Bang241002 <bang241002@gmail.com>
2023-08-07 20:56:52 +0700

    bai thi

--- demo/src/main/java/com/example/bai_thi/demo/EmployeeBean.java
+++ demo/src/main/java/com/example/bai_thi/demo/EmployeeBean.java
@@ -1,17 +1,12 @@
 package com.example.bai_thi.demo;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-
-
-@WebServlet("/employee")
-
-public class wed {
+public class EmployeeBean {
     private EmployeeBean employeeBean;
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
