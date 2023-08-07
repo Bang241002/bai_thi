09cbbcc8c3c22372e514289a7510bc6b1cac021f
Bang241002 <bang241002@gmail.com>
2023-08-07 20:53:14 +0700

    bai thi

--- demo/src/main/java/com/example/bai_thi/demo/wed.java
+++ demo/src/main/java/com/example/bai_thi/demo/wed.java
@@ -1,23 +1,29 @@
 package com.example.bai_thi.demo;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
+
+
+@WebServlet("/employee")
+
 public class wed {
+    private EmployeeBean employeeBean;
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String name = request.getParameter("name");
         double wage = Double.parseDouble(request.getParameter("wage"));
-        employeeBean.createEmployee(name, wage);
+        employeeBean.Employee(name, wage);
         response.sendRedirect(request.getContextPath() + "/employeeList");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        List<Employee> employeeList = employeeBean.findAllEmployees();
+        List<Employee> employeeList = employeeBean.Employees();
         request.setAttribute("employeeList", employeeList);
         request.getRequestDispatcher("/WEB-INF/views/employeeList.jsp").forward(request, response);
     }
